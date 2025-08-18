8e19794c3519dadd89ebbb1a1eb88f508eee93d5
js9467 <64589684+js9467@users.noreply.github.com>
2025-08-18 17:47:54 -0400

    Update route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,9 @@
-import { auth } from "@/auth";
-import { db } from "@/lib/db";
+
+import { auth } from "../../../src/auth";
+import { db } from "../../../src/lib/db";
 import { z } from "zod";
+
+
 
 const CreateBody = z.object({
   kind: z.enum(["TEXT","PHOTO","VIDEO"]),
